a46e0a68541149c51acde18f15fcb5bffcb1b499
SauravKumar <sk729584@gmail.com>
2024-11-11 09:25:20 +0530

    Added a spinner component

---
+++ components/ui/spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="w-6 h-6 border-4 border-t-4 border-solid rounded-full animate-spin"></div>
+  );
+};
+
+export default Spinner;
